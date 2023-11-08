4dff843101ef3d32f140c87ab7ca88583c1c42bf
Marius Bughiu <marius.bughiu@gmail.com>
2023-11-08 21:14:18 +0200

    Add sample application

---
+++ samples/Samples/Foo.Bar.SampleApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Foo.Bar.SampleApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ samples/Samples/Foo.Bar.SampleApp/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Plugin.AdMob;
+using Plugin.AdMob.Configuration;
+
+namespace Foo.Bar.SampleApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            AdConfig.UseTestAdUnitIds = true;
+
+            var builder = MauiApp.CreateBuilder();
+
+            builder
+                .UseMauiApp<App>()
+                .UseAdMobHandlers()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
