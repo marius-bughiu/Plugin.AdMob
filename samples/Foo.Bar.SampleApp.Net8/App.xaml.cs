fb73160b30de99243a25155a6d5e124f0d296824
Marius Bughiu <marius.bughiu@uipath.com>
2025-01-25 10:37:35 +0200

    Clean up + fix SupportedOSPlatformVersion

--- samples/Foo.Bar.SampleApp.Net8/App.xaml.cs
+++ samples/Foo.Bar.SampleApp.Net8/App.xaml.cs
@@ -1,12 +1,14 @@
-﻿namespace Foo.Bar.SampleApp
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+﻿namespace Foo.Bar.SampleApp;
 
-            MainPage = new AppShell();
-        }
-    }
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
 }
