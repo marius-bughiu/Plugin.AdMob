fd4837a408a755af211c000eeeea72c1b3de4f71
Marius Bughiu <marius.bughiu@gmail.com>
2025-03-15 01:11:39 +0200

    blank native ad service

---
+++ src/Plugin.AdMob/Services/NativeAdService.cs
@@ -0,0 +1,35 @@
+﻿using Plugin.AdMob.Configuration;
+
+namespace Plugin.AdMob.Services;
+
+/// <summary>
+/// A service used for managing interstitial ads.
+/// </summary>
+public interface INativeAdService
+{
+
+}
+
+internal class NativeAdService(IAdConsentService _adConsentService) 
+    : INativeAdService
+{
+    private static string? GetAdUnitId(string? adUnitId)
+    {
+        if (AdConfig.UseTestAdUnitIds)
+        {
+            return AdMobTestAdUnits.Interstitial;
+        }
+
+        return adUnitId ?? AdConfig.DefaultInterstitialAdUnitId;
+    }
+
+    private bool CanRequestAds()
+    {
+        if (AdConfig.DisableConsentCheck)
+        {
+            return true;
+        }
+
+        return _adConsentService.CanRequestAds();
+    }
+}
